4f5abf0c22bd20be39f259727dc275c559489f89
Jay Lord P. Torres <isens.jaylord@gmail.com>
2022-01-08 04:49:47 +0800

    user sample data

---
+++ data/users.ts
@@ -0,0 +1,119 @@
+export const users = [
+  {
+    id: "bed8245f-d456-4576-9ba1-4cc48b7fa79b",
+    fullName: "William V. Murray",
+    address: "1299 Tanglewood Road Memphis, MS 38117",
+    note: "sample note 1",
+    avatarUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: "137a5a8b-20c3-42a2-ace0-655f7fb45930",
+    fullName: "Paul W. Wheeler",
+    note: "sample note 2",
+    address: "3383 Daylene Drive Ann Arbor, MI 48104",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
+  },
+  {
+    id: "d9efe108-390b-4f44-8d16-f2b1418f29b5",
+    fullName: "Grace J. Garcia",
+    note: "sample note 3",
+    address: "595 Tree Frog Lane Lenexa, MO 66215",
+    avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
+  },
+  {
+    id: "857e520c-4749-44d8-9238-b92a3fc8094d",
+    fullName: "Frank D. Lawrence",
+    note: "sample note 4",
+    address: "2574 Timbercrest Road Hooper Bay, AK 99604",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
+  },
+  {
+    id: "3568c10c-e2d1-4551-b185-2f29b08efe64",
+    fullName: "Renee R. Arnold",
+    note: "sample note 5",
+    address: "3358 Meadow Drive Billings, MT 59101",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
+  },
+  {
+    id: "bed8245f-d456-4576-9ba1-4cc48b7fa79b",
+    fullName: "William V. Murray",
+    address: "1299 Tanglewood Road Memphis, MS 38117",
+    note: "sample note 1",
+    avatarUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: "137a5a8b-20c3-42a2-ace0-655f7fb45930",
+    fullName: "Paul W. Wheeler",
+    note: "sample note 2",
+    address: "3383 Daylene Drive Ann Arbor, MI 48104",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
+  },
+  {
+    id: "d9efe108-390b-4f44-8d16-f2b1418f29b5",
+    fullName: "Grace J. Garcia",
+    note: "sample note 3",
+    address: "595 Tree Frog Lane Lenexa, MO 66215",
+    avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
+  },
+  {
+    id: "857e520c-4749-44d8-9238-b92a3fc8094d",
+    fullName: "Frank D. Lawrence",
+    note: "sample note 4",
+    address: "2574 Timbercrest Road Hooper Bay, AK 99604",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
+  },
+  {
+    id: "3568c10c-e2d1-4551-b185-2f29b08efe64",
+    fullName: "Renee R. Arnold",
+    note: "sample note 5",
+    address: "3358 Meadow Drive Billings, MT 59101",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
+  },
+  {
+    id: "bed8245f-d456-4576-9ba1-4cc48b7fa79b",
+    fullName: "William V. Murray",
+    address: "1299 Tanglewood Road Memphis, MS 38117",
+    note: "sample note 1",
+    avatarUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: "137a5a8b-20c3-42a2-ace0-655f7fb45930",
+    fullName: "Paul W. Wheeler",
+    note: "sample note 2",
+    address: "3383 Daylene Drive Ann Arbor, MI 48104",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
+  },
+  {
+    id: "d9efe108-390b-4f44-8d16-f2b1418f29b5",
+    fullName: "Grace J. Garcia",
+    note: "sample note 3",
+    address: "595 Tree Frog Lane Lenexa, MO 66215",
+    avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
+  },
+  {
+    id: "857e520c-4749-44d8-9238-b92a3fc8094d",
+    fullName: "Frank D. Lawrence",
+    note: "sample note 4",
+    address: "2574 Timbercrest Road Hooper Bay, AK 99604",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
+  },
+  {
+    id: "3568c10c-e2d1-4551-b185-2f29b08efe64",
+    fullName: "Renee R. Arnold",
+    note: "sample note 5",
+    address: "3358 Meadow Drive Billings, MT 59101",
+    avatarUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
+  },
+];
