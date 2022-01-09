9b567d4609848b37e7982bacf13657d519916659
Jay Lord P. Torres <isens.jaylord@gmail.com>
2022-01-09 20:37:10 +0800

    groupForm text events

--- screens/group/component/GroupForm.tsx
+++ screens/group/component/GroupForm.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { Avatar, VStack, Input, Button } from "native-base";
+import React, { useCallback, useRef, useState } from "react";
+import { Avatar, VStack, Input, Button, Text } from "native-base";
 
 export default function GroupForm({ action, navigation }: any) {
+  const nameRef = useRef();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const onCreate = useCallback(() => {
+    alert(name + description);
+  }, [name, description, nameRef]);
+
   return (
     <VStack space={1} m="4">
       <VStack space={1} alignItems="center">
         <Avatar size="200px" />
       </VStack>
-      <Input size="md" placeholder="Name" />
-      <Input size="md" placeholder="Description" />
+      <Input size="md" placeholder="Name" value={name} onChangeText={setName} />
+      <Text>{name}</Text>
+      <Input
+        size="md"
+        placeholder="Description"
+        value={description}
+        onChangeText={setDescription}
+      />
 
-      {action === "update" ? <Button>Update</Button> : <Button>Create</Button>}
+      {action === "update" ? (
+        <Button>Update</Button>
+      ) : (
+        <Button onPress={onCreate}>Create</Button>
+      )}
     </VStack>
   );
 }
