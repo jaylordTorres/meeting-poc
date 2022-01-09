295ec015bb9e250e97f607977cccb9af89f14531
Jay Lord P. Torres <isens.jaylord@gmail.com>
2022-01-10 04:12:26 +0800

    fix typo

--- components/Info.tsx
+++ components/Info.tsx
@@ -1,11 +1,21 @@
 import { HStack, Spacer, Text } from "native-base";
 
-export default function Info({ title, info }: { title: String; info: String }) {
+export default function Info({
+  title,
+  value,
+  field,
+  isEdit = false,
+}: {
+  isEdit?: boolean;
+  title: String;
+  value: String;
+  field?: any;
+}) {
   return (
     <HStack>
       <Text>{title}</Text>
       <Spacer />
-      <Text>{info}</Text>
+      {isEdit ? field : <Text>{value}</Text>}
     </HStack>
   );
 }
